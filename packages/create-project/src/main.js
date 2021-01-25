2ec440585de48ffcfc28ff7016a294bf37fe949a
Naci Akce <naci_akce@hotmail.com>
2021-01-25 17:01:06 +0100

    fix(create-frontend-project): change  access check of template dir

--- packages/create-project/src/main.js
+++ packages/create-project/src/main.js
@@ -41,21 +41,8 @@ export async function createProject(options) {
         '../../templates',
         options.template.toLowerCase(),
     );
-    console.log('templatee', templateDir);
-    options.templateDirectory = templateDir;
 
-    try {
-        await access(
-            templateDir,
-            fs.constants.R_OK || fs.constants.W_OK,
-        );
-    } catch (err) {
-        console.log(
-            '%s Invalid template name',
-            kleur.bold().red('ERROR'),
-        );
-        process.exit(1);
-    }
+    options.templateDirectory = templateDir;
 
     const tasks = new Listr([
         {
