61eb92fe5765186dfbe7a50cdab6df735d1c66ed
Naci Akce <naci_akce@hotmail.com>
2021-01-28 21:04:15 +0100

    fix(scripts): import webpack.config  from the project root

--- packages/scripts/src/webpack/webpack.config.js
+++ packages/scripts/src/webpack/webpack.config.js
@@ -1,15 +1,18 @@
 import fs from 'fs';
 import { merge } from 'webpack-merge';
+import { createRequire } from 'module';
+
 import base from './webpack.base.js';
 import dev from './webpack.dev.js';
 import prod from './webpack.prod.js';
 import { isProd, webpackConfig } from './config/index.js';
 
-const localWebpackConfig = fs.existsSync(webpackConfig)
-    ? webpackConfig
-    : {};
+const require = createRequire(import.meta.url);
 
 export default () => {
     const config = isProd ? prod : dev;
+    const localWebpackConfig = fs.existsSync(webpackConfig)
+        ? require(webpackConfig)
+        : {};
     return merge(base, config, localWebpackConfig);
 };
