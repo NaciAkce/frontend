8030c3741d935d9c719b7db9d08119a9aa9618bd
Naci Akce <naci_akce@hotmail.com>
2021-01-31 20:02:14 +0100

    feat(scripts): check if windows

--- packages/scripts/src/webpack/config/env.js
+++ packages/scripts/src/webpack/config/env.js
@@ -1,10 +1,13 @@
 import dotenvExpand from 'dotenv-expand';
 import dotenv from 'dotenv';
 import { env } from './paths.js';
+import isWindows from 'is-windows';
 
 const NODE_ENV = process.env.NODE_ENV;
 
 if (!NODE_ENV) throw new Error('NODE_ENV is not defined');
+
+const devServerHost = isWindows() ? '127.0.0.1' : '0.0.0.0';
 
 const dotenvFile = `${env}.${NODE_ENV}`;
 
@@ -17,8 +20,8 @@ dotenvExpand(
 export const isProd = process.env.NODE_ENV === 'production';
 export const isDev = !isProd;
 export const port = parseInt(process.env.PORT, 10) || 3000;
-export const host = process.env.HOST || '0.0.0.0';
+export const host = process.env.HOST || devServerHost;
 export const prettyHost =
-    host === '0.0.0.0' || host === '::' ? 'localhost' : host;
+    host === devServerHost || host === '::' ? 'localhost' : host;
 export const protocol =
     process.env.HTTPS === 'true' ? 'https' : 'http';
